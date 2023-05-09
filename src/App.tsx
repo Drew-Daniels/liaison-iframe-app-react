207b3582071477b264ca417f0fbd3e9ae48d11f9
Drew Daniels <daniels_drew@protonmail.com>
2023-05-08 21:36:24 -0500

    upgrade liaison-react

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,17 @@ import { useIFrame } from 'liaison-react';
 import './App.css'
 
 function App() {
-  const [logoutRequests, setLogoutRequests] = useState(0);
+  const [loggedIn, setLoggedIn] = useState(false);
   const [tokens, setTokens] = useState<Array<string>>([]);
 
   const { callParentEffect } = useIFrame({
     parentOrigin: 'http://localhost:3003',
     effects: {
-      logout: () => setLogoutRequests(prevNum => prevNum + 1),
-      saveToken: ({ args: { token } }) => {
-        setTokens(prevTokens => [...prevTokens, token]);
+      onLogoutRequested: ({ callParentEffect }) => {
+        localStorage.remove('token');
+        callParentEffect({ name: 'onLogoutCompleted' });
       },
-      saveTokenAsync: ({ args: { token } }) => {
+      onTokenReceived: ({ args: { token } }) => {
         setTokens(prevTokens => [...prevTokens, token]);
       },
     }
@@ -24,26 +24,19 @@ function App() {
       <h1>Child Window</h1>
       <div className="buttons">
         <p className="buttons-header">Request to run events within the Parent window!</p>
-        <button onClick={initiateParentLogout} className="btn">Initiate <em>Parent</em> Logout Process</button>
         <button onClick={requestTokenFromParent} className="btn">Request Token from Parent (Get Synchronously)</button>
         <button onClick={requestTokenFromParentAsync} className="btn">Request Token from Parent (Get Asynchronously)</button>
       </div>
       <div>
-        <h2>Logout Requests:</h2>
-        <p>{logoutRequests}</p>
-        <h2>Tokens:</h2>
-        <ul>
-          {tokens.map((token) => <li key={token}>Token: {token}</li>)}
-        </ul>
+        {!loggedIn &&
+          <p>You're not logged in...</p>
+        }
+        {loggedIn &&
+          <p>You're logged in!</p>
+        }
       </div>
     </>
   )
-
-  function initiateParentLogout() {
-    callParentEffect({
-      name: 'logout',
-    })
-  }
 
   function requestTokenFromParent() {
     callParentEffect({
